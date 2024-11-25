9780759d691225fc388f3606f7e87fa2eadd4f87
thiago-laza <thiago.profmat@gmail.com>
2024-11-25 20:17:38 -0300

    Aula 02

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+#from fastapi.responses import HTMLResponse
+
+from fast_zero.achemas import Message
 
 app = FastAPI()
 
-
-@app.get('/')
+'''
+Resposta em HTML
+@app.get('/', response_class=HTMLResponse)
 def read_root():
-    return {'message': 'Olá Mundo2!'}
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+'''
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'mesege': 'ola mundo'}
