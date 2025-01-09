8bc3d709ee387e4f565403f8b82160904378dced
thiago-laza <thiago.profmat@gmail.com>
2025-01-09 20:35:28 -0300

    PC-01: exercicios da aula 04 com ajustes

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,12 +1,25 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fast_zero.models import User
 
 
-def test_create_user(session):
-    user = User(username='test', email='test@test.com', password='secret')
-    session.add(user)
-    session.commit()
-    result = session.scalar(select(User).where(User.email == 'test@test.com'))
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
+        session.add(new_user)
+        session.commit()
 
-    assert result.username == 'test'
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'alice',
+        'password': 'secret',
+        'email': 'teste@test',
+        'created_at': time,
+        'updated_at': time,  # Exercício
+    }
